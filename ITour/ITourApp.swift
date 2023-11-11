ee743a6aaa12780bdede2373bcb74371b4ea04b6
David Grammatico <david.lugdunum@gmail.com>
2023-11-11 19:22:24 +0100

    Initial Commit

---
+++ ITour/ITourApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITourApp.swift
+//  ITour
+//
+//  Created by David Grammatico on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ITourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
