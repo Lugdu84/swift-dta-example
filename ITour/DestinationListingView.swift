447473b0828ecd529c6fd7c57c0f45a150f9c48d
David Grammatico <david.lugdunum@gmail.com>
2023-11-11 22:47:52 +0100

    Add DestinationListingView and Sight files.

---
+++ ITour/DestinationListingView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationListingView.swift
+//  ITour
+//
+//  Created by David Grammatico on 11/11/2023.
+//
+
+import SwiftUI
+
+struct DestinationListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationListingView()
+}
---
+++ ITour/Sight.swift
@@ -0,0 +1,8 @@
+//
+//  Sight.swift
+//  ITour
+//
+//  Created by David Grammatico on 11/11/2023.
+//
+
+import Foundation
